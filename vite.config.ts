6037d0dd4d092d1258026c8c08810567a0a2db83
ulyscd <ulyscd@gmail.com>
2026-06-09 01:41:55 -0700

    Configure custom domain for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ulys.dev/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
